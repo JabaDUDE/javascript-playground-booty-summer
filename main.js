d99e26a220e5183082c523fc408c60c0f2a7e0d2
JabaDUDE <dakotawkeast@gmail.com>
2022-08-01 18:47:56 -0400

    add global variables

---
+++ main.js
@@ -0,0 +1,5 @@
+const darthVader = document.querySelector(".darth-vader");
+const darthSidious = document.querySelector(".darth-sidious");
+const darthPlagueis = document.querySelector(".darth-plagueis-the-wise");
+const darthRaven = document.querySelector(".darth-raven");
+const darthBane = document.querySelector(".darth-bane");
