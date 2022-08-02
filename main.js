5b6fad4b750eeca6bef98321918728d2e19265c6
JabaDUDE <dakotawkeast@gmail.com>
2022-08-01 22:05:57 -0400

    add eventlisteners

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ const container = document.querySelector("#container");
 darthVader.addEventListener("click", function () {
   container.innerHTML = `<h4>Luke, I am your baby momma's daddy</h4>`;
 });
+darthSidious.addEventListener("click", function () {
+  container.innerHTML = `<h4>Have you heard the tale of Darth Plagueis the Wise?</h4>`;
+});
+darthPlagueis.addEventListener("click", function () {
+  container.innerHTML = `<h4>Idk if I was that wise, my apprentice killed me in my sleep.</h4>`;
+});
+darthRaven.addEventListener("click", function () {
+  container.innerHTML = `<h4>I have multiple personality disorder and spent my life very confused.</h4>`;
+});
+darthBane.addEventListener("click", function () {
+  container.innerHTML = `<h4>I was the imposter all along.</h4>`;
+});
