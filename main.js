a25a6b3da5af7e2ac7aafb0998e12c24fff35451
JabaDUDE <dakotawkeast@gmail.com>
2022-08-01 20:54:57 -0400

    add messages

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const darthSidious = document.querySelector(".darth-sidious");
 const darthPlagueis = document.querySelector(".darth-plagueis-the-wise");
 const darthRaven = document.querySelector(".darth-raven");
 const darthBane = document.querySelector(".darth-bane");
+const container = document.querySelector("#container");
+
+darthVader.addEventListener("click", function () {
+  container.innerHTML = `<h4>Luke, I am your baby momma's daddy</h4>`;
+});
